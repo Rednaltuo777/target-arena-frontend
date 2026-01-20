2e58c7593bff5025e72b259ddab1a73cb06bb8dd
Fjodor Isaev <rednaltuo@gmail.com>
2026-01-20 21:02:06 +0100

    Test simple app

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,8 @@
-import { useState } from "react";
-import AuthCallback from "./authCallback";
-import Login from "./components/Login";
-import UserBooking from "./UserBooking";
-
-function App() {
-  const [user, setUser] = useState(null);
-
+export default function App() {
   return (
-    <>
-      <AuthCallback onLogin={setUser} />
-      {user ? <UserBooking user={user} onLogout={() => setUser(null)} /> : <Login />}
-    </>
+    <div style={{ padding: "40px", textAlign: "center", background: "#0f172a", color: "white", minHeight: "100vh", display: "flex", alignItems: "center", justifyContent: "center" }}>
+      <h1>🎯 Target Arena funkar!</h1>
+      <p>React är igång</p>
+    </div>
   );
 }
-
-export default App;
